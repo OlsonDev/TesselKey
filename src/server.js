da68678ece4aa2e958dd1b46303bda6617a6f4d3
Josh Olson <Olson.dev@Gmail.com>
2013-09-08 22:56:25 -0500

    Allow compression and whitespace trimming.

--- src/server.js
+++ src/server.js
@@ -15,14 +15,13 @@ var os      = require('os')
 app.disable('x-powered-by');
 
 app.configure(function() {
+	app.use(express.compress());
 	app.use(trimmer());
-	//app.use(express.compress());
 	app.use(express.favicon());
 	app.use('/images',  express.static(images));
 	app.use('/scripts', express.static(scripts));
 	app.use(less({ src: pub, dest: tmp }));
 	app.use(express.static(tmp));
-	//app.use(trimmer());
 	app.use(express.static(views));
 });
 
