e5ae0b584a300d102532ba3aa05fdb82f329ae40
Josh Olson <Olson.dev@Gmail.com>
2013-09-25 23:37:13 -0500

    Clean up trimmer module for readability

--- src/modules/trimmer.js
+++ src/modules/trimmer.js
@@ -1,34 +1,38 @@
 function getSize(chunk) {
 	return Buffer.isBuffer(chunk)
-	? chunk.length
-	: Buffer.byteLength(chunk);
+		? chunk.length
+		: Buffer.byteLength(chunk)
+	;
+}
+
+function trimHtml(chunk, encoding) {
+	return chunk
+		.toString(encoding)
+		.replace(/>\s+</g, '><')
+		.replace(/\s{2,}/g, ' ')
+	;
 }
 
 module.exports = function trimmer() {
 	return function trimmer(req, res, next) {
-		var write = res.write
-			, isHtml
-		;
+		var write = res.write;
+
+		function rewrite(chunk, encoding) {
+			var html = trimHtml(chunk, encoding)
+				, buffer = new Buffer(html, encoding)
+			;
+			res.setHeader('Content-Length', getSize(buffer));
+			return write.call(res, buffer, encoding);
+		}
 
 		res.write = function(chunk, encoding) {
 			var type = res.getHeader('Content-Type') || ''
-				, html
+				, isHtml = type.indexOf('text/html') >= 0
 			;
-			isHtml = type.indexOf('text/html') >= 0;
-			if (!isHtml) {
-				write.apply(res, arguments);
-				return;
-			}
-
-			html = chunk
-				.toString(encoding)
-				.replace(/>\s+</g, '><')
-				.replace(/\s{2,}/g, ' ')
+			return isHtml
+				? rewrite(chunk, encoding)
+				: write.apply(res, arguments)
 			;
-
-			var buffer = new Buffer(html, encoding);
-			res.setHeader('Content-Length', getSize(buffer));
-			return write.call(res, buffer, encoding);
 		};
 
 		next();
