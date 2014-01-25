877005355f62153f64f0be236364f353c5d4bb88
Josh Olson <Olson.dev@Gmail.com>
2014-01-25 17:00:22 -0600

    Complete refocus of project
    Previously this project behaved as a sort of documentation. However,
    Technical Machine made many changes to their Tessel prototype. To
    prevent having to constantly update this project, I've decided to remove
    the documentation aspect of this project and instead focus solely on the
    keyboard functionality. Besides, their documentation has improved
    substantially. This commit removes the documentation-like code and lays
    the ground work for the keyboard "admin."

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -2,88 +2,12 @@
 /*global _ */
 (function() {
 	'use strict';
-	var vm = {
-			board: {
-				name: 'Tessel'
-				, chips: buildChips()
-				, switches: buildSwitches()
-				, leds: buildLEDs()
-				, pinGroups: buildPinGroups()
-			}
-		}
+	var vm = {}
 		, mapping = {
-			  chips:     { create: function(o) { return new Chip    (o.data, o.parent); } }
-			, switches:  { create: function(o) { return new Switch  (o.data, o.parent); } }
-			, leds:      { create: function(o) { return new LED     (o.data, o.parent); } }
-			, pins:      { create: function(o) { return new Pin     (o.data, o.parent); } }
-			, pinGroups: { create: function(o) { return new PinGroup(o.data, o.parent); } }
-			, board:     { create: function(o) { return new Board   (o.data, o.parent); } }
+			  layouts:  { create: function(o) { return new Layout (o.data, o.parent); } }
+			, profiles: { create: function(o) { return new Profile(o.data, o.parent); } }
 		}
 	;
-
-	function buildChips() {
-		return _.map('Microcontroller|Flash|RAM|Wi-Fi'.split('|'), function(name, i) {
-			return { name: name, description: name };
-		});
-	}
-
-	function buildSwitches() {
-		return _.map('Reset button|SimpleLink Wi-Fi SmartConfig button'.split('|'), function(desc, i) {
-			var name = 'S' + (i + 1);
-			return { name: name, description: desc || name };
-		});
-	}
-
-	function buildLEDs() {
-		return _.map('Power LED|Wi-Fi Connection LED|Wi-Fi Error LED|Debug LED 1|Debug LED 2'.split('|'), function(desc, i) {
-			var name = 'D' + (i + 1);
-			return { name: name, description: desc || name };
-		});
-	}
-
-	function buildPinGroups() {
-		var groups = _.map('GPIO Bank|A|B|C|D'.split('|'), function(name, i) {
-				var inches = i ? '0.1' : '0.2'
-					, mm = i ? '2.54' : '5.08'
-					, pinNumFactor = !i * 2
-					, pinNumOffset = !i * -1
-				;
-				return {
-					name: name
-					, description: name.length === 1 ? 'Module ' + name : name
-					, numPins: name.length === 1 ? '10 pins (1x10)' : '20 pins (2x10)'
-					, pins: buildDefaultPins(inches, mm, pinNumFactor, pinNumOffset)
-				};
-			})
-			, gpio = groups[0]
-		;
-		gpio.pins = buildGPIOPins().concat(gpio.pins);
-		return groups;
-	}
-
-	function buildPins(names, inches, mm, pinNumFactor, pinNumOffset) {
-		pinNumFactor = pinNumFactor || 1;
-		pinNumOffset = pinNumOffset || 0;
-		return _.map(names.split('|'), function(name, i) {
-			return {
-				name: name
-				, description: name
-					.replace(/^A(\d)$/, 'ADC$1')
-					.replace(/^G(\d)$/, 'GPIO$1')
-				, inchesToEdge: inches || '0.1'
-				, mmToEdge: mm || '2.54'
-				, pinNum: (i + 1) * pinNumFactor + pinNumOffset
-			};
-		});
-	}
-
-	function buildDefaultPins(inches, mm, pinNumFactor, pinNumOffset) {
-		return buildPins('GND|3V3|SCL|SDA|CLK|MISO|MOSI|G1|G2|G3', inches, mm, pinNumFactor, pinNumOffset);
-	}
-
-	function buildGPIOPins(inches, mm) {
-		return buildPins('G6|G5|G4|A5|A4|A3|A2|A1|A0|5V', inches, mm, 2);
-	}
 
 	function init(data, parent) {
 		var self = this;
@@ -103,66 +27,49 @@
 		self.hovered = ko.observable(false);
 	}
 
-	function Chip() {
+	function Layout() {
 		var self = this;
 		init.apply(self, arguments);
+		self.numRows = ko.observable(10);
+		self.numColumns = ko.observable(10);
+		self.keys = ko.observableArray();
 	}
 
-	function Switch() {
+	function LayoutKey() {
 		var self = this;
 		init.apply(self, arguments);
+		self.row = ko.observable(0);
+		self.column = ko.observable(0);
 	}
 
-	function LED() {
+	function USBKey() {
 		var self = this;
 		init.apply(self, arguments);
+		self.fullName = ko.observable('');
+		self.displayName = ko.observable('');
+		self.code = ko.observable(0);
 	}
 
-	function Pin(data, pinGroup) {
+	function Profile() {
 		var self = this;
 		init.apply(self, arguments);
-		self.tooltipTmpl = ko.computed(function() {
-			var code = self.code()
-				, pinGroupCode = 'pin-group-' + _.slugify(pinGroup.name())
-			;
-			return '#' + pinGroupCode + '-' + code + '-tooltip-tmpl, #' + code + '-tooltip-tmpl, #' + self.typeCode() + '-tooltip-tmpl';
-		});
-		self.fullName = ko.computed(function() {
-			var name = self.name()
-				, desc = self.description()
-			;
-			return name === desc ? name : name + ' (' + desc + ')';
-		});
-	}
-
-	function PinGroup() {
-		var self = this;
-		init.apply(self, arguments);
-		self.hasHoveredChild = ko.computed(function() {
-			return !!self.pins.firstByProperty('hovered', true);
-		});
-	}
-
-	function Board() {
-		var self = this
-			, w = 1977
-			, h = 2360
-		;
-		init.apply(self, arguments);
-		self.scale = ko.observable(0.24);
-		self.rotation = ko.observable(0);
-		self.width  = ko.computed(function() { return self.scale() * w + 'px'; });
-		self.height = ko.computed(function() { return self.scale() * h + 'px'; });
-		self.rotationDeg = ko.computed(function() { return self.rotation() + 'deg'; });
-		self.transform = ko.computed(function() {
-			return 'rotate(' + self.rotationDeg() + ')';
-		});
 	}
 
 	function ViewModel() {
-		var self = this;
+		var self = this
+			, wl = window.location
+			, hash = wl.hash.replace(/^#/, '')
+			, tabMatch = hash.match(/^(?:matrix|layout|profile|keys-available)$/)
+			, tab = tabMatch ? tabMatch[0] : 'matrix'
+		;
 		init.apply(self, arguments);
 		self.ready = ko.observable(false);
+		self.tab = ko.observable(tab);
+		self.keysAvailable = ko.observableArray();
+		self.addKeyAvailable = function() {
+			self.keysAvailable.push(new USBKey);
+		};
+		if (hash && !tabMatch) wl.hash = '';
 	}
 
 	window.vm = vm = new ViewModel(vm);
